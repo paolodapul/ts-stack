2bbd31d8416340b26707a04ecc4890571549825e
Paolo Dapul <paolodapultech@gmail.com>
2024-11-26 15:19:36 +0800

    test: undo insert and delete

--- main_test.ts
+++ main_test.ts
@@ -30,3 +30,17 @@ Deno.test("`nothing to undo` should be logged", () => {
   assertSpyCallArg(logSpy, 0, 0, "Nothing to undo.")
   logSpy.restore()
 })
+
+Deno.test("undo insert", () => {
+  const editor = new TextEditor()
+  editor.insert("Hello")
+  editor.insert(" World")
+  let content = editor.getContent()
+  expect(content).toBe("Hello World")
+  editor.undo()
+  content = editor.getContent()
+  expect(content).toBe("Hello")
+  editor.delete(5)
+  editor.undo()
+  expect(content).toBe("Hello")
+})
