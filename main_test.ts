5a0c42cccc9ad6683e5fe1cdf2ffc9be8c5b085e
Paolo Dapul <paolodapultech@gmail.com>
2024-11-26 15:00:30 +0800

    test:  should be logged

--- main_test.ts
+++ main_test.ts
@@ -1,4 +1,5 @@
 import { expect } from "@std/expect"
+import { assertSpyCallArg, spy } from "@std/testing/mock";
 import { TextEditor } from "./main.ts";
 
 Deno.test("get empty textcontent upon instantiation", () => {
@@ -20,4 +21,12 @@ Deno.test("delete inserted text", () => {
   editor.delete(5)
   const content = editor.getContent()
   expect(content).toBe("")
+})
+
+Deno.test("`nothing to undo` should be logged", () => {
+  const editor = new TextEditor()
+  const logSpy = spy(console, "log")
+  editor.undo()
+  assertSpyCallArg(logSpy, 0, 0, "Nothing to undo.")
+  logSpy.restore()
 })
